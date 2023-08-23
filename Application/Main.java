82c74d4002fdaf4fbfee0575c0502ca15de8604c
Lucas Martins <lucasfranca_zs@hotmail.com>
2023-08-22 21:39:12 -0300

    Instanciado as entidades Post e Comments com base do programa e realizado testes com primeiro post

--- Application/Main.java
+++ Application/Main.java
@@ -1,18 +1,31 @@
 package Application;
 
-import java.util.Locale;
-import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import Entities.Comment;
+import Entities.Post;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
+		Comment c1 = new Comment("Have a nice trip!");
+		Comment c2 = new Comment("Wow that´s awesome!");
 		
+		Post p1 = new Post(
+				sdf.parse("22/08/2023 21:26:44"), 
+				"Traveling to New Zeeland",
+				"I´m going to visit this wonderful country!",
+				12);
 		
-		sc.close();
+		p1.addComment(c1);
+		p1.addComment(c2);
+		
+		System.out.println(p1);
+		
 	}
 
 }
